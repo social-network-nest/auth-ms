f0b24b78cf4cc98c399160073ccd6fc9cd037ddf
YTheSumsito <marcelo.jara@gmail.com>
2025-04-17 00:44:54 -0400

    add user-ms

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,13 +1,17 @@
 import { Controller } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { MessagePattern } from '@nestjs/microservices';
+import { UserService } from 'src/user/user.service';
 
 @Controller()
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(
+    private readonly authService: AuthService,
+    private readonly userService: UserService
+  ) {}
 
   @MessagePattern({ cmd: 'login_user' })
-  login() {
-    return 'como estas mi amorcito user';
+  loginUser() {
+    return this.userService.listUsers();
   }
 }
