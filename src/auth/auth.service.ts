df3eb13095182c84b4226281cef6415d0060dc4c
YTheSumsito <marcelo.jara@gmail.com>
2025-04-21 00:59:18 -0400

    add bycrypt

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,5 +1,6 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService extends PrismaClient implements OnModuleInit {
@@ -12,7 +13,14 @@ export class AuthService extends PrismaClient implements OnModuleInit {
             email: 'mjaral@outlook.com',
             password: '123456',
         }
-        const user = await this.auth.create({data});
+        const saltRounds = 10;
+        const hashedPassword = await bcrypt.hash(data.password, saltRounds);
+        const user = await this.auth.create({
+            data: {
+                email: data.email,
+                password: hashedPassword,
+            },
+        });
         return user;
     }
 }
